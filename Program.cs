040033f4e89bca4b4d3af04031ca682645d83610
Michael Anderson <Micmail566@gmail.com>
2017-01-16 18:15:34 -0500

    need to fix game retry.

--- Program.cs
+++ Program.cs
@@ -9,79 +9,123 @@ namespace MysteryWord2
 {
     class Program
     {
-        
+
 
         static void Main(string[] args)
         {
-            string[] pulledWordArray = File.ReadAllLines(@"..\..\words.txt");
-            List<string> pulledWordList = pulledWordArray.ToList<string>();
-
-            var rng = new Random();
-            int randomName = rng.Next(pulledWordList.Count);
-            string randomWord = pulledWordList[randomName];
-
-            int counter = 0;
-            Console.WriteLine(randomWord);
-            foreach (char place in randomWord)
+            bool retry = true;
+            while (retry)
+                retry = false;
             {
-                Console.Write("_ ");
-                counter++;
-            }
-            counter = 0;
-            int turns = randomName;
-            int rWordLength = randomWord.Length;
-            List<char> usedLetters = new List<char>();
-            char[] output = new char[rWordLength];
-            bool winCheck = false;
-            while (!winCheck && turns > 0)
-            {
-               
+
+                string[] pulledWordArray = File.ReadAllLines(@"..\..\words.txt");
+                List<string> pulledWordList = pulledWordArray.ToList<string>();
+
+                var rng = new Random();
+                int randomName = rng.Next(pulledWordList.Count);
+                string randomWord = pulledWordList[randomName];
+
+                int counter = 0;
+                Console.WriteLine(randomWord);
+                foreach (char place in randomWord)
+                {
+                    Console.Write("_ ");
+                    counter++;
+                }
+                counter = 0;
+
+                int rWordLength = randomWord.Length;
+                int turns = rWordLength;
+                List<char> usedLetters = new List<char>();
+                char[] output = new char[rWordLength];
+                bool winCheck = true;
+                while (winCheck && turns > 0)
+                {
+
                     string letter = Console.ReadLine();
+                    Console.Clear();
                     char input = letter[0];
                     if (usedLetters.Contains(input))
                     {
                         usedLetters.Add(input);
-                        Console.WriteLine("You've already tried '{0}',", input);
+                        Console.WriteLine("You've already used '{0}',", input);
                         continue;
                     }
-                if (randomWord.Contains(input))
-                {
-                    usedLetters.Add(input);
-
-                    for (int i = 0; i < rWordLength; i++)
+                    if (randomWord.Contains(input))
                     {
-                        if (randomWord[i] == input)
-                        {
-                            output[i] = randomWord[i];
-                            counter++;
+                        usedLetters.Add(input);
 
+                        for (int countLength = 0; countLength < rWordLength; countLength++)
+                        {
+                            if (randomWord[countLength] == input)
+                            {
+                                output[countLength] = randomWord[countLength];
+                                counter++;
+
+                            }
+                        }
+                        string output2 = new string(output);
+                        if (output2 == randomWord)
+                        {
+                            winCheck = false;
                         }
                     }
-                    if (counter == rWordLength)
-                        winCheck = true;
+                    else
+                    {
+                        usedLetters.Add(input);
+                        Console.WriteLine("Wrong", input);
+                        turns--;
+                    }
+                    Console.WriteLine(randomWord);
+                    Console.WriteLine(output);
+                    Console.WriteLine($"Your Have {turns} Trys Left");
 
+                    foreach (char place in randomWord)
+                    {
+                        Console.Write("_ ");
+                        counter++;
+                    }
+                    if (winCheck == false)
+                    {
+                        Console.WriteLine("winner");
+                        Console.WriteLine("Play agian Y/N ?");
+                        string playAgainCK = Console.ReadLine();
+                        if (playAgainCK == "Y")
+                        {
+                            retry = true;
+                        }
+                        else if (playAgainCK == "N")
+                        {
+                            Console.WriteLine("Press Enter to exit");
+                            retry = false;
+                        }
+                    }
+                    else if (turns == 0)
+                    {
+                        Console.WriteLine("Loser");
+                        Console.WriteLine("Play agian Y/N ?");
+                        string playAgainCK = Console.ReadLine();
+                        if (playAgainCK == "Y")
+                        {
+                            retry = true;
+                        }
+                        else if (playAgainCK == "N")
+                        {
+                            Console.WriteLine("Press Enter to exit");
+                            retry = false;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Incorrect Entery");
+                            retry = false;
+                        }
+                    }
                 }
-                else
-                {
-                    usedLetters.Add(input);
-                    Console.WriteLine("Wrong", input);
-                    turns--;
-                }
-                Console.WriteLine(output);
-
-                
 
             }
-            if (winCheck)
-            {
-                Console.WriteLine("winner");
-            }
-            else
-            {
-                Console.WriteLine("Loser");
-                Console.Write("enter to exit");
-                Console.ReadLine();
-            }
+
+
+
 
         }
     }
